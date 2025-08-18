92c4af5d0a4cea556a9f4c7c9b3f87f328e3fada
rnflrnfl41 <120080932+rnflrnfl41@users.noreply.github.com>
2025-08-18 16:16:05 +0900

    feat: basic setting

---
+++ metro.config.js
@@ -0,0 +1,22 @@
+const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
+
+const config = getDefaultConfig(__dirname);
+
+// Alias 설정
+config.resolver.alias = {
+  '@': path.resolve(__dirname),
+  '@app': path.resolve(__dirname, 'app'),
+  '@components': path.resolve(__dirname, 'components'),
+  '@features': path.resolve(__dirname, 'features'),
+  '@shared': path.resolve(__dirname, 'shared'),
+  '@store': path.resolve(__dirname, 'store'),
+  '@assets': path.resolve(__dirname, 'assets'),
+  '@constants': path.resolve(__dirname, 'constants'),
+  '@hooks': path.resolve(__dirname, 'hooks'),
+  '@utils': path.resolve(__dirname, 'shared/utils'),
+  '@types': path.resolve(__dirname, 'shared/types'),
+  '@services': path.resolve(__dirname, 'shared/services'),
+};
+
+module.exports = config;
